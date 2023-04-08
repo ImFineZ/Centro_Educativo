d79772ab8b81fc727330f3d3b3dcf1cd6ec29ef7
esibaja0795 <sibaja0795@gmail.com>
2023-04-07 22:12:16 -0600

    -Pantallas principales despues de validar el login -Se incluyó validacion de valores null en el login mostrando un alert por credenciales vacias o incorrectas

--- programa/model/model.php
+++ programa/model/model.php
@@ -13,35 +13,33 @@ class model
     }
 
     public function m_Validar_Login($obj_u)
-    {   
-        
-        
+    {
+
+
         $useremail = $this->ins_conexion->escapar($obj_u->get_useremail()); // Prevenir SQL injection
         $pass = $this->ins_conexion->escapar($obj_u->get_pass());
-        
+
         $sql = "SELECT id_usuario, nombre, apellido, email, pass, tipo_usu, estado FROM usuarios WHERE email='$useremail' AND pass='$pass' LIMIT 1"; // Agregar LIMIT 1 para optimizar la consulta
-        
+
         $rs = $this->ins_conexion->ejecutar($sql);
-        
+
         if (!$rs) {
             // Manejo de errores
             return null;
         }
-        
+
         $row = mysqli_fetch_assoc($rs);
-        
+
         if (!$row) {
             // Las credenciales no son válidas
             return null;
         }
-        
+
         // Asignar los valores obtenidos del registro a $obj_u
-        
+
         $obj_u->set_id_usuario($row['id_usuario']);
         $obj_u->set_nombre($row['nombre']);
         $obj_u->set_apellido($row['apellido']);
-        
-        
         $obj_u->set_tipo_usu($row['tipo_usu']);
         $obj_u->set_estado($row['estado']);
 
